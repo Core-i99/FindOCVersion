72dfad92ed79fc0c689a71d26fc659231c780c43
Core i99 <thehackguy@icloud.com>
2022-05-22 13:20:59 +0200

    1

--- macOS-build/setup.py
+++ macOS-build/setup.py
@@ -9,7 +9,6 @@ APP = ['FindOCVersion.py']
 DATA_FILES = []
 OPTIONS = {
     'iconfile': 'macOS-build/Icon.icns',
-    'packages': ['tkinter'],
     'plist': {
         'CFBundleDevelopmentRegion': 'English',
         'CFBundleIdentifier': "com.Core-i99.FindOCVersion",
