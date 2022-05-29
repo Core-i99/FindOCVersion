4781029d02952755bb56fb10a494fcb45c20d41b
Core i99 <thehackguy@icloud.com>
2022-05-29 14:53:40 +0200

    Create setup.py

---
+++ macOS-build/setup.py
@@ -0,0 +1,25 @@
+"""
+Usage:
+    python setup.py py2app
+"""
+
+from setuptools import setup
+
+APP = ['FindOCVersion.py']
+DATA_FILES = []
+OPTIONS = {
+    'iconfile': 'resources/Icon.icns',
+    'plist': {
+        'CFBundleDevelopmentRegion': 'English',
+        'CFBundleIdentifier': "com.Core-i99.FindOCVersion",
+        'CFBundleVersion': "2.0",
+        'NSHumanReadableCopyright': "Copyright © 2022, Stijn Rombouts, All Rights Reserved"
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
